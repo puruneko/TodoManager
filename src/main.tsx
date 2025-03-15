b751cb1ec6be89f8b164c6744dd18b09650c2af1
prog.puru.neko.cat <prog.puru.neko.cat@gmail.com>
2025-03-15 19:46:48 +0900

    fullcalenderのお試し(イベント登録・削除)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
+import Calender from "./calender/calender"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <p>main.tsx</p>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+            <div>
+                <p>main.tsx</p>
+                <App />
+            </div>
+            <div style={{ margin: "100px 50px 0 50px", maxWidth: "90vw" }}>
+                <Calender />
+            </div>
+        </div>
+    </StrictMode>
 )
